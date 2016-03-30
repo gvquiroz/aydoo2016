08fcdb67c13e362f9ea00641d82fb1ef40941d7d
gvquiroz <gvquiroz@gmail.com>
2016-03-29 18:38:48 -0800

    Nueva funcionalidad con nuevo parametro
    Agregado nuevo parametro para forma de devolucion de valores

--- factores-primos-2/src/main/java/ar/edu/untref/aydoo/CalculadoraDePrimos.java
+++ factores-primos-2/src/main/java/ar/edu/untref/aydoo/CalculadoraDePrimos.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 public class CalculadoraDePrimos {
 	
 	ArrayList<Integer> listaDeNumerosPrimos = new ArrayList<Integer>();
-	public void calcularPrimos(int numero){
-		
+	
+	/*
+	 * Calcula calculadora descomposicion de numeros primos y los guarda en un Arraylist
+	 */
+	public void calcularPrimos(int numero){	
 	    for(int i=2;i<=numero;i++){
 	        while(numero%i==0){
 	            numero=numero/i;
@@ -14,6 +17,10 @@ public class CalculadoraDePrimos {
 	        }
 	    }
 	}
+	
+	/*
+	 * Devuelve el array con numeros calculados en calcularPrimos
+	 */
 	public ArrayList<Integer> getListaDeNumerosPrimos(){
 		return listaDeNumerosPrimos;
 	}
--- factores-primos-2/src/main/java/ar/edu/untref/aydoo/Program.java
+++ factores-primos-2/src/main/java/ar/edu/untref/aydoo/Program.java
@@ -14,15 +14,40 @@ public class Program {
 	            System.out.println("El valor de entrada debe ser un Integer");
 	            System.exit(1);
 	        }
+	        
+	        
 	        CalculadoraDePrimos Calculadora = new CalculadoraDePrimos();
 	        Calculadora.calcularPrimos(numero);
 	        ArrayList<Integer> resultado  = Calculadora.getListaDeNumerosPrimos();
-	        System.out.print("Factores primos: " + numero + ": ");
+
 			Iterator<Integer> it = resultado.iterator();
 			
-			while (it.hasNext()){
-				System.out.print(it.next() + " ");
-			}
+			/*
+			 * Switch que reconoce el tipo de formato para la entrega de valores
+			 */
+	        switch (args[1]) {
+	         	case "--format=pretty":
+	 		    System.out.print("Factores primos: " + numero + ": ");
+	 			while (it.hasNext()){
+	 				System.out.print(it.next() + " ");
+				}
+	             break;
+	         case "--format=quiet":
+	 		     System.out.print("Factores primos: " + numero + ": ");
+	        	 System.out.println();
+	        	 
+	        	 /*
+	        	  * Utilizado un for inverso para cambiar el orden de entrega de valores segun consigna
+	        	  */
+	        	 
+	        	 for(int j = resultado.size() - 1; j >= 0; j--){
+	        		 System.out.println(resultado.get(j));
+	        	}
+	        	 break;
+	         default:
+	        	 System.out.println("Formato no aceptado. Las opciones posibles son: pretty o quiet.");
+	     }
+
 			System.out.println();
 	    }
 }
